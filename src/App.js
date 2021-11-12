9b3a2a42e9a34a0c40a51f62188376da5b79b921
jek701 <ikromhmd@gmail.com>
2021-11-12 11:23:45 +0500

    Added Bellissimo Audio to git

--- src/App.js
+++ src/App.js
@@ -1,51 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import H5AudioPlayer from "react-h5-audio-player";
-import * as Id3 from 'id3js';
-import playBtn from './play.png';
-import pauseBtn from './pause.png'
-// import getAudioDurationInSeconds from "get-audio-duration";
 
-let links = ['https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3']
+// let links = ['https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Wish%20You%20Were%20Gay.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Idontwannabeyouanymore.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20Ocean%20Eyes.mp3', 'https://music.iodev.uz/audio/Billie%20Eilish%20-%20My%20Strange%20Addiction.mp3']
+//
+// // Adding new el after every two el in "links" arr
+//
+// links = links.reduce((acc, val, key) => {
+//     if (key % 2)
+//         return [...acc, val, 'https://music.iodev.uz/audio/Belissimo_voice_over.mp3']
+//     return [...acc, val]
+// }, [])
+//
+// // Adding new el after every two el in "links" arr
 
-// Adding new el after every two el in "links" arr
-
-links = links.reduce((acc, val, key) => {
-    if (key % 2)
-        return [...acc, val, 'https://music.iodev.uz/audio/Belissimo_voice_over.mp3']
-    return [...acc, val]
-}, [])
-
-// Adding new el after every two el in "links" arr
-
-const PlaylistBlock = ({id, active, onclick, src, duration}) => {
-
-    const [musicData, setMusicData] = useState([])
-
-    // getAudioDurationInSeconds(links[0]).then((duration) => {
-    //     console.log(duration);
-    // });
-
-    // Getting data of music
-
-    useEffect(() => {
-        Id3.fromUrl(`${src}`).then((tags) => {
-            setMusicData(tags);
-        });
-    }, [src]);
-
-    if (musicData.length === 0) {
-        return null;
-    }
-
-    // Getting data of music
+const PlaylistBlock = ({id, active, onclick, src, duration, artist, title, img}) => {
 
     return (
         <div onClick={onclick} className={`playlist-block ${id} ${active}`}>
             <p className='id'><strong>{id}</strong></p>
-            <p className='title'>{musicData.title}</p>
-            <p className='artist-name'>{musicData.artist}</p>
+            <img className='cover' src={img} alt="" width={40}/>
+            <p className='title'>{title}</p>
+            <p className='artist-name'>{artist}</p>
             <p className='duration'>{duration}</p>
-            <audio src={links[id]}/>
+            <audio src={src}/>
+        </div>
+    )
+}
+
+const NowPlaying = ({ cover, artist, title }) => {
+    return (
+        <div className='curr-music-wrapper'>
+            <img src={'data:image/png;base64,' + cover} alt=""/>
+            <div className="text">
+                <h1>{artist}</h1>
+                <h3>{title}</h3>
+            </div>
         </div>
     )
 }
@@ -55,50 +44,56 @@ const App = () => {
     const [apiData, setApiData] = useState([])
 
     useEffect(() =>{
-        fetch(`https://music.iodev.uz/audio/`, {
-            headers : {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-
+        fetch(`https://audio.iodev.uz/public/music`, {
         })
             .then((response) => response.json())
             .then(e => setApiData(e))
-    })
+        })
 
     const [currMusic, setCurrMusic] = useState(0);
 
     return <>
+        {apiData.length > 0 ?
+            <NowPlaying artist={apiData[currMusic].artist} title={apiData[currMusic].title} cover={apiData[currMusic].cover} />
+            :
+            null
+        }
         <div className='playlist-wrapper'>
             <div className='header-title'>
                 <li>#</li>
+                <li>Cover</li>
                 <li>Title</li>
                 <li>Artist</li>
+                <li>Duration</li>
             </div>
-            {links.map((i, index) => {
+            {apiData.map((i, index) => {
                 return <PlaylistBlock
-                    duration={links[index].duration}
-                    src={links[index]}
+                    duration={i.playtime}
+                    src={'https://audio.iodev.uz/' + i.url}
                     onclick={() => setCurrMusic(index)}
-                    id={index + 1} active={currMusic === index ? 'active' : ''}/>
+                    id={index + 1} active={currMusic === index ? 'active' : ''}
+                    title={i.title}
+                    artist={i.artist}
+                    img={'data:image/png;base64,' + i.cover}
+                />
             })}
 
         </div>
-        <H5AudioPlayer
+        {apiData.length > 0 ? <H5AudioPlayer
             loop={false}
             showSkipControls={true}
             showJumpControls={false}
-            autoPlay={false}
+            autoPlay={true}
             preload={"auto"}
-            src={links[currMusic]}
+            src={'https://audio.iodev.uz/' + apiData[currMusic].url}
             onPlay={() => console.log("onPlay")}
-            onEnded={() => currMusic === (links.length - 1) ? setCurrMusic(0) : setCurrMusic(currMusic + 1)}
-            onClickNext={() => currMusic === (links.length - 1) ? setCurrMusic(0) : setCurrMusic(currMusic + 1)}
+            onEnded={() => currMusic === (apiData.length - 1) ? setCurrMusic(0) : setCurrMusic(currMusic + 1)}
+            onClickNext={() => currMusic === (apiData.length - 1) ? setCurrMusic(0) : setCurrMusic(currMusic + 1)}
             onClickPrevious={() => currMusic === 0 ? setCurrMusic(0) : setCurrMusic(currMusic - 1)}
             hasDefaultKeyBindings={true}
             muted={false}
             layout="stacked-reverse"
-        />
+        /> : null}
     </>
 };
 
